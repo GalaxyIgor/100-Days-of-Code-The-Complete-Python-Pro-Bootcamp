784fd765b7dbee90acd26340446ba78a9e7d7e83
GalaxyIgor <igornbelisario@gmail.com>
2025-04-23 18:32:59 -0300

    att

---
+++ Day 1/task/task.py
@@ -0,0 +1,8 @@
+# Pause 1 = Question(What is your name?) = numeber of characters
+print(len(input("What is your name?")))
+
+# Pause 2
+name = input("What is your name?")
+leght = len(name)
+print("The number of characters are " + leght)
+
