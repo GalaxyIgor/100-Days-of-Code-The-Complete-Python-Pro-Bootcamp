821d90ac1a17910dd862acd7d4ceb3075ef90699
Igor <igornbelisario@gmail.com>
2026-07-07 21:55:11 -0300

    Update main.py

--- Beginner/Day - 010 - Beginner - Functions With Outputs/Notebook/The Calculator Project/main.py
+++ Beginner/Day - 010 - Beginner - Functions With Outputs/Notebook/The Calculator Project/main.py
@@ -17,21 +17,28 @@ operation = {
     "/": divide
 }
 
-print("Welcome to the calculator program!")
-# print(operation["+"](2, 3))
+def calculator():
+    print("Welcome to the calculator program!")
+    # print(operation["+"](2, 3))
 
-num1 = float(input("What is the first number?: "))
-input_operation = input("What operation would you like to perform? (+, -, *, /): ")
-num2 = float(input("What is the second number?: "))
-answer = operation[input_operation](num1, num2)
-print(f"{num1} {input_operation} {num2} = {answer}")
-input_continue = input(f"Type 'y' to continue calculation with {answer} or type 'n' to exit: ")
+    num1 = float(input("What is the first number?: "))
+    
+    should_continue = True
+    while should_continue:
+        input_operation = input("What operation would you like to perform? (+, -, *, /): ")
+        num2 = float(input("What is the second number?: "))
+        
+        answer = operation[input_operation](num1, num2)
+        print(f"{num1} {input_operation} {num2} = {answer}")
+    
+        input_continue = input(f"Type 'y' to continue calculation with {answer} or type 'n' to exit: ")
 
-if input_continue == "y":
-    num1 = answer
-    input_operation = input("What operation would you like to perform? (+, -, *, /): ")
-    num2 = float(input("What is the second number?: "))
-    answer = operation[input_operation](num1, num2)
-    print(f"{num1} {input_operation} {num2} = {answer}")
-else:
-    print("Goodbye!")
+        if input_continue == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            print("\n" * 20)
+            calculator()
+            return
+
+calculator()
