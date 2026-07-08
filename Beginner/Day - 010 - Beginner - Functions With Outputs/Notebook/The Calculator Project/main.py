5f0ded0c4fc68596c11f59261700965ae903a6cc
Igor <igornbelisario@gmail.com>
2026-07-07 21:35:03 -0300

    feat: cap 10 calculator project

---
+++ Beginner/Day - 010 - Beginner - Functions With Outputs/Notebook/The Calculator Project/main.py
@@ -0,0 +1,37 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operation = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print("Welcome to the calculator program!")
+# print(operation["+"](2, 3))
+
+num1 = float(input("What is the first number?: "))
+input_operation = input("What operation would you like to perform? (+, -, *, /): ")
+num2 = float(input("What is the second number?: "))
+answer = operation[input_operation](num1, num2)
+print(f"{num1} {input_operation} {num2} = {answer}")
+input_continue = input(f"Type 'y' to continue calculation with {answer} or type 'n' to exit: ")
+
+if input_continue == "y":
+    num1 = answer
+    input_operation = input("What operation would you like to perform? (+, -, *, /): ")
+    num2 = float(input("What is the second number?: "))
+    answer = operation[input_operation](num1, num2)
+    print(f"{num1} {input_operation} {num2} = {answer}")
+else:
+    print("Goodbye!")
